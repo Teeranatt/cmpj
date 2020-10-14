98d2b6256e616c0997ea42f5435ace0bc7786916
Teeranatt <teeranat.sr.61@ubu.ac.th>
2020-10-14 21:43:22 +0700

    Updates

---
+++ precache-manifest.7f86510712a21a09375fe360d00ca77b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31546de61636be0b221bee431a84985c",
+    "url": "/cmpj/index.html"
+  },
+  {
+    "revision": "cba4542c1fe9a2f621a8",
+    "url": "/cmpj/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "bc026afa3352575e27e6",
+    "url": "/cmpj/static/css/main.0236d509.chunk.css"
+  },
+  {
+    "revision": "cba4542c1fe9a2f621a8",
+    "url": "/cmpj/static/js/2.f0dd6d56.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cmpj/static/js/2.f0dd6d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc026afa3352575e27e6",
+    "url": "/cmpj/static/js/main.b53f0734.chunk.js"
+  },
+  {
+    "revision": "cb54c49a7f6e39a665fe",
+    "url": "/cmpj/static/js/runtime-main.895084b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cmpj/static/media/logo.5d5d9eef.svg"
+  }
+]);
